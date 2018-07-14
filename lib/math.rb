6ba6579ba9db4841f40b49dc5e8caf1a46bdb622
Natasha K <kapr.natasha@gmail.com>
2018-07-14 19:39:28 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,11 +1,14 @@
 def addition(num1, num2)
-  return num1 + num2
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
+  
 end
 
 def multiplication(num1, num2)
